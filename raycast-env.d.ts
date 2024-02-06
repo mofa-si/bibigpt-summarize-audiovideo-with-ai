1f9399640ac61e9ec926c579bfe3b87615a2135b
hiskyline <hiskyline@evergreenai.cn>
2024-02-06 20:41:22 +0800

    feat: add support for multiple languages

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -15,8 +15,10 @@ declare type Preferences = ExtensionPreferences
 declare namespace Preferences {
   /** Preferences accessible in the `summarizemedia` command */
   export type Summarizemedia = ExtensionPreferences & {
-  /** API Token URL - 请输入 API Token链接 */
-  "apiTokenUrl": string
+  /** API Token URL - Please enter the API Token URL */
+  "apiTokenUrl": string,
+  /** Output Language - Please slelect the language for the summary */
+  "outputLanguage": "en-US" | "zh-CN" | "zh-TW" | "ja-JP" | "it-IT" | "de-DE" | "es-ES" | "fr-FR" | "nl-NL" | "ko-KR" | "km-KH" | "hi-IN"
 }
 }
 
